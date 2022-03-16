73817ac19ecc735891343205f9056dddf43c3d5e
Juan Manuel Gimeno Illa <jmgimeno@diei.udl.cat>
2022-03-16 19:18:51 +0100

    solved some exercices on either

--- src/main/scala/fpinscala/exercises/errorhandling/Either.scala
+++ src/main/scala/fpinscala/exercises/errorhandling/Either.scala
@@ -8,18 +8,43 @@ enum Either[+E,+A]:
   case Left(get: E)
   case Right(get: A)
 
-  def map[B](f: A => B): Either[E, B] = ???
+  def map[B](f: A => B): Either[E, B] =
+    this match
+      case Left(e) => Left(e)
+      case Right(a) => Right(f(a))
 
-  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = ???
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] =
+    this match
+      case Right(a) => f(a)
+      case Left(e) => Left(e) // tipus Either[EE, B]
+      // case l => l que no funciona
+      // l: Either[E, A] però necessites és Either[EE, B]
+      // EE >: E  es compleix que Either[EE, B] >: Either[E, B]
+      // un Either[E, B] és un Either[EE, B]
+      // Per construir un Either[E, B] hi ha dues possibilitats:
+      //    - Right(b) a partir de b: B
+      //    - Left(e) a partir d'un e: E
 
-  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = ???
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] =
+    this match
+      case Left(_) => b
+      case r => r       // Either[EE, B] >: Either[E, A]
 
-  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = ???
+  def map2[EE >: E, B, C](eb: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
+    flatMap(a => eb.map(b => f(a, b)))
+
+  def map2_2[EE >: E, B, C](eb: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
+    for
+      a <- this
+      b <- eb
+    yield f(a, b)
 
 object Either:
-  def traverse[E,A,B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] = ???
+  def traverse[E,A,B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] =
+    es.foldRight(Right(Nil):Either[E, List[B]])((a, acc) => f(a).map2(acc)(_ :: _))
 
-  def sequence[E,A](es: List[Either[E,A]]): Either[E,List[A]] = ???
+  def sequence[E,A](es: List[Either[E,A]]): Either[E,List[A]] =
+    traverse(es)(identity)
 
   def mean(xs: IndexedSeq[Double]): Either[String, Double] = 
     if xs.isEmpty then
