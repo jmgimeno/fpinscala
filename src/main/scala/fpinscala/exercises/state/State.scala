97002da6f79148017e097b7986fd5608446b6c36
Juan Manuel Gimeno Illa <jmgimeno@diei.udl.cat>
2022-03-29 18:32:45 +0200

    solved some problems on RNGs

--- src/main/scala/fpinscala/exercises/state/State.scala
+++ src/main/scala/fpinscala/exercises/state/State.scala
@@ -7,7 +7,7 @@ trait RNG:
 object RNG:
   // NB - this was called SimpleRNG in the book text
 
-  case class Simple(seed: Long) extends RNG:
+  case class Simple(seed: Long) extends RNG :
     def nextInt: (Int, RNG) =
       val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL // `&` is bitwise AND. We use the current seed to generate a new seed.
       val nextRNG = Simple(newSeed) // The next state, which is an `RNG` instance created from the new seed.
@@ -27,17 +27,60 @@ object RNG:
       (f(a), rng2)
     }
 
-  def nonNegativeInt(rng: RNG): (Int, RNG) = ???
+  def nonNegativeInt(rng: RNG): (Int, RNG) =
+    val (i, rng2) = rng.nextInt
+    val r = if i < 0 then -(i + 1) else i
+    (r, rng2)
 
-  def double(rng: RNG): (Double, RNG) = ???
+  def double(rng: RNG): (Double, RNG) =
+    val (i, rng2) = nonNegativeInt(rng)
+    val f = i / (Int.MaxValue.toDouble + 1)
+    (f, rng2)
 
-  def intDouble(rng: RNG): ((Int,Double), RNG) = ???
+  def intDouble(rng: RNG): ((Int, Double), RNG) =
+    val (i, rng2) = rng.nextInt
+    val (h, rng3) = double(rng2)
+    ((i, h), rng3)
 
-  def doubleInt(rng: RNG): ((Double,Int), RNG) = ???
+  def doubleInt(rng: RNG): ((Double, Int), RNG) =
+    val (h, rng2) = double(rng)
+    val (i, rng3) = rng2.nextInt
 
-  def double3(rng: RNG): ((Double,Double,Double), RNG) = ???
+    ((h, i), rng3)
 
-  def ints(count: Int)(rng: RNG): (List[Int], RNG) = ???
+  def doubleInt_2(rng: RNG): ((Double, Int), RNG) =
+    val ((i, h), rng2) = intDouble(rng)
+    ((h, i), rng2)
+
+  def doubleInt_3(rng: RNG): ((Double, Int), RNG) =
+    val iD = intDouble(rng)
+    ((iD._1._2, iD._1._1), iD._2)
+
+  def double3(rng: RNG): ((Double, Double, Double), RNG) =
+    val (d1, rng2) = double(rng)
+    val (d2, rng3) = double(rng2)
+    val (d3, rng4) = double(rng3)
+    ((d1, d2, d3), rng4)
+
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) =
+    if count > 0 then
+      val (i, rng2) = rng.nextInt
+      val (list, rng3) = ints(count - 1)(rng2)
+      (i :: list, rng3)
+    else (Nil, rng)
+
+  def ints_2(count: Int)(rng: RNG): (List[Int], RNG) =
+    // The order of generation of the elements in the list
+    // is the reverse of ints
+    @annotation.tailrec
+    def go(count: Int, rng: RNG, acc: List[Int]): (List[Int], RNG) =
+      if count == 0 then (acc, rng)
+      else {
+        val (i, rng2) = rng.nextInt
+        go(count - 1, rng2, i :: acc)
+      }
+    go(count, rng, Nil)
+
 
   def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
 
@@ -52,7 +95,7 @@ object RNG:
 opaque type State[S, +A] = S => (A, S)
 
 object State:
-  extension [S, A](underlying: State[S, A])
+  extension[S, A] (underlying: State[S, A])
     def run(s: S): (A, S) = underlying(s)
 
     def map[B](f: A => B): State[S, B] =
