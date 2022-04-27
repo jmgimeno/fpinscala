3c490a18e8c9cd82d32c3bbcde7aafc81d7eb84c
Juan Manuel Gimeno Illa <jmgimeno@gmail.com>
2022-04-27 11:49:20 +0200

    prepared session on property-based testing

--- src/main/scala/fpinscala/exercises/testing/Gen.scala
+++ src/main/scala/fpinscala/exercises/testing/Gen.scala
@@ -3,26 +3,64 @@ package fpinscala.exercises.testing
 import fpinscala.answers.state.*
 
 import java.util.concurrent.{ExecutorService, Executors}
+import scala.annotation.targetName
 
 /*
 The library developed in this chapter goes through several iterations. This file is just the
 shell, which you can fill in and modify while working through the chapter.
 */
 
-trait Prop:
-  self => // self reference to the receiving object in order to
-          // use it inside check and not produce infinite recursion
-    def check: Boolean
-    def &&(that: Prop): Prop =
-      new Prop:
-        def check: Boolean = self.check && that.check
+import Prop.*
+import Result.*
+
+opaque type Prop = (TestCases, RNG) => Result
 
 object Prop:
-  def forAll[A](gen: Gen[A])(f: A => Boolean): Prop = ???
+
+  opaque type TestCases = Int
+  opaque type SuccessCount = Int
+  opaque type FailedCase = String
+
+  enum Result:
+    case Passed
+    case Falsified(failure: FailedCase, successes: SuccessCount)
+
+    def isFalsified: Boolean = this match
+      case Passed => false
+      case Falsified(_, _) => true
+
+  end Result
+
+  def forAll[A](as: Gen[A])(f: A => Boolean): Prop =
+    (n, rng) => randomLazyList(as)(rng).zip(LazyList.from(0)).take(n).map {
+      case (a, i) =>
+        try
+          if f(a) then Passed else Falsified(a.toString, i)
+        catch
+          case e: Exception => Falsified(buildMsg(a, e), i)
+    }.find(_.isFalsified).getOrElse(Passed)
+
+  def randomLazyList[A](g: Gen[A])(rng: RNG): LazyList[A] =
+    LazyList.unfold(rng)(rng => Some(g.run(rng)))
+
+  def buildMsg[A](s: A, e: Exception): String =
+    s"test case: $s\n" +
+      s"generated an exception: ${e.getMessage}\n" +
+      s"stack trace:\n ${e.getStackTrace.mkString("\n")}"
+
+  extension (self: Prop)
+    def &&(that: Prop): Prop = ???
+    def ||(that: Prop): Prop = ???
+
+  @targetName("forAllSized")
+  def forAll[A](g: SGen[A])(f: A => Boolean): Prop = ???
+
+end Prop
 
 opaque type Gen[+A] = State[RNG, A]
 
 object Gen:
+
   def unit[A](a: => A): Gen[A] =
     State.unit(a)
 
@@ -34,8 +72,10 @@ object Gen:
                                     //^^^^^^^ RNG => (Boolean, RNG)
               // vvvv State[RNG, A]
   extension [A](self: Gen[A])
+
     def listOfN(n: Int): Gen[List[A]] =
       State.sequence(List.fill(n)(self))
+
   //def flatMap[B](f: A => State[RNG, B]): State[RNG, B] =
     def flatMap[B](f: A => Gen[B]): Gen[B] =
       State.flatMap(self)(f)      //self.flatMap(f) infinite recursion
@@ -43,10 +83,17 @@ object Gen:
       //   companion object as a regular method of the object
       //   passing the "extended object" in a first parameter
       //   list
+
     def listOfN(size: Gen[Int]): Gen[List[A]] =
       size.flatMap(listOfN)
 
-    def sample(rng: RNG): A = self.run(rng)._1
+    def sample(rng: RNG): A = self.run(rng)._1 // Added for tests in the worksheet
+
+    def unsized: SGen[A] = ???
+
+    def list: SGen[List[A]] = ???
+
+  end extension
 
   def union[A](g1: Gen[A], g2: Gen[A]): Gen[A] =
     boolean.flatMap(b => if b then g1 else g2)
@@ -54,4 +101,8 @@ object Gen:
   def weighted[A](g1: (Gen[A], Double), g2: (Gen[A], Double)): Gen[A] =
     val probG1 = g1._2.abs / (g1._2.abs + g2._2.abs)
     State(RNG.double).flatMap(d => if d < probG1 then g1._1 else g2._1)
+
+end Gen
+
+opaque type SGen[+A] = Int => Gen[A]
 
