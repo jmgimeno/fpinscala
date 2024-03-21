0261ea695e2510e52a0833537613366bcba79cb1
Juan Manuel Gimeno Illa <jmgimeno@diei.udl.cat>
2024-03-21 18:50:43 +0100

    after march 21

--- src/main/scala/fpinscala/exercises/laziness/LazyList.scala
+++ src/main/scala/fpinscala/exercises/laziness/LazyList.scala
@@ -4,40 +4,77 @@ enum LazyList[+A]:
   case Empty
   case Cons(h: () => A, t: () => LazyList[A])
 
-  def toList: List[A] = ???
+  import LazyList.*
+
+  def toList: List[A] =
+    this match
+      case Empty            => Nil
+      case Cons(head, tail) => head() :: tail().toList
 
   // The book only defines the second argument as passed by name
-  def foldRight[B](z: => B)(f: (=> A, => B) => B): B = // The arrow `=>` in front of the argument type `B` means that the function `f` takes its second argument by name and may choose not to evaluate it.
+  def foldRight[B](
+      z: => B
+  )(
+      f: (=> A, => B) => B
+  ): B = // The arrow `=>` in front of the argument type `B` means that the function `f` takes its second argument by name and may choose not to evaluate it.
     this match
-      case Cons(h,t) => f(h(), t().foldRight(z)(f)) // If `f` doesn't evaluate its second argument, the recursion never occurs.
+      case Cons(h, t) =>
+        f(
+          h(),
+          t().foldRight(z)(f)
+        ) // If `f` doesn't evaluate its second argument, the recursion never occurs.
       case _ => z
 
-  def exists(p: A => Boolean): Boolean = 
-    foldRight(false)((a, b) => p(a) || b) // Here `b` is the unevaluated recursive step that folds the tail of the lazy list. If `p(a)` returns `true`, `b` will never be evaluated and the computation terminates early.
+  def exists(p: A => Boolean): Boolean =
+    foldRight(false)((a, b) =>
+      p(a) || b
+    ) // Here `b` is the unevaluated recursive step that folds the tail of the lazy list. If `p(a)` returns `true`, `b` will never be evaluated and the computation terminates early.
 
   @annotation.tailrec
   final def find(f: A => Boolean): Option[A] = this match
-    case Empty => None
+    case Empty      => None
     case Cons(h, t) => if (f(h())) Some(h()) else t().find(f)
 
-  def take(n: Int): LazyList[A] = ???
+  // retorna LazyList[A] amb els n primers elements de la llista i si acaba empty
+  def take(n: Int): LazyList[A] =
+    println(s"take $n")
+    this match
+      case Cons(h, t) if n > 0 => cons(h(), t().take(n - 1))
+      case _                   => empty
 
-  def drop(n: Int): LazyList[A] = ???
+  // retorna LazyList[A] eliminant els n elements (si els te) per inici o empty
+  def drop(n: Int): LazyList[A] =
+    println(s"drop $n")
+    this match
+      case Cons(_, t) if n > 0 => t().drop(n - 1)
+      case _ => this
 
-  def takeWhile(p: A => Boolean): LazyList[A] = ???
+  // retorna LazyList[A] mentres es compleixi la propietat
+  def takeWhile(p: A => Boolean): LazyList[A] =
+    println("take while")
+    this match
+      case Cons(h, t) if p(h()) => cons(h(), t().takeWhile(p))
+      case _ => empty
 
   def forAll(p: A => Boolean): Boolean = ???
 
-  def headOption: Option[A] = ???
+  def headOption: Option[A] =
+    this match
+      case Empty => None
+      case Cons(h, t) => Some(h())
+
+  def tailOption: Option[LazyList[A]] =
+    this match
+      case Empty => None
+      case Cons(h, t) => Some(t())
 
   // 5.7 map, filter, append, flatmap using foldRight. Part of the exercise is
   // writing your own function signatures.
 
   def startsWith[B](s: LazyList[B]): Boolean = ???
 
-
 object LazyList:
-  def cons[A](hd: => A, tl: => LazyList[A]): LazyList[A] = 
+  def cons[A](hd: => A, tl: => LazyList[A]): LazyList[A] =
     lazy val head = hd
     lazy val tail = tl
     Cons(() => head, () => tail)
@@ -46,7 +83,7 @@ object LazyList:
 
   // apply creates a LazyList but it's eager !!!
   def apply[A](as: A*): LazyList[A] =
-    if as.isEmpty then empty 
+    if as.isEmpty then empty
     else cons(as.head, apply(as.tail*))
 
   val ones: LazyList[Int] = LazyList.cons(1, ones)
--- src/main/scala/worksheets/lazylist.worksheet.sc
+++ src/main/scala/worksheets/lazylist.worksheet.sc
@@ -5,4 +5,35 @@ val l_eager: LazyList[Int] = LazyList(1, sys.error("boom"), 2)
 
 val l_lazy: LazyList[Int] = cons(1, cons(sys.error("boom"), cons(2, empty)))
 
+def f(n: Int): Int =
+  println(s"f $n")
+  n
 
+val lazyList = cons(f(1), cons(f(2), cons(f(3), cons(f(4), empty))))
+val normalList = f(1) :: f(2) :: f(3) :: f(4) :: Nil
+
+lazyList.headOption
+
+lazyList.headOption
+
+val firstThree = lazyList.take(3)
+
+firstThree.tailOption
+
+firstThree.tailOption
+
+firstThree.tailOption.flatMap(_.headOption)
+
+val lazyList2 = cons(f(1), cons(f(2), cons(f(3), cons(f(4), empty))))
+
+val afterThree = lazyList2.drop(3)
+
+afterThree.headOption
+
+val lazyList3 = cons(f(1), cons(f(2), cons(f(3), cons(f(4), empty))))
+
+val untilThree = lazyList3.takeWhile(_ < 3)
+
+untilThree.headOption
+
+untilThree.tailOption
