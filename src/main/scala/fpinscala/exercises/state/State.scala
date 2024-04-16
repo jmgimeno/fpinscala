9def507ba4dfac0a559025b4dadf44d0b30303e2
Juan Manuel Gimeno Illa <jmgimeno@gmail.com>
2024-04-16 16:29:32 +0200

    after april 16

--- src/main/scala/fpinscala/exercises/state/State.scala
+++ src/main/scala/fpinscala/exercises/state/State.scala
@@ -1,6 +1,5 @@
 package fpinscala.exercises.state
 
-
 trait RNG:
   def nextInt: (Int, RNG) // Should generate a random `Int`. We'll later define other functions in terms of `nextInt`.
 
@@ -9,10 +8,63 @@ object RNG:
 
   case class Simple(seed: Long) extends RNG:
     def nextInt: (Int, RNG) =
-      val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL // `&` is bitwise AND. We use the current seed to generate a new seed.
-      val nextRNG = Simple(newSeed) // The next state, which is an `RNG` instance created from the new seed.
-      val n = (newSeed >>> 16).toInt // `>>>` is right binary shift with zero fill. The value `n` is our new pseudo-random integer.
-      (n, nextRNG) // The return value is a tuple containing both a pseudo-random integer and the next `RNG` state.
+      val newSeed =
+        (seed * 0x5deece66dL + 0xbL) & 0xffffffffffffL // `&` is bitwise AND. We use the current seed to generate a new seed.
+      val nextRNG = Simple(
+        newSeed
+      ) // The next state, which is an `RNG` instance created from the new seed.
+      val n =
+        (newSeed >>> 16).toInt // `>>>` is right binary shift with zero fill. The value `n` is our new pseudo-random integer.
+      (
+        n,
+        nextRNG
+      ) // The return value is a tuple containing both a pseudo-random integer and the next `RNG` state.
+
+  // -2^31 ..0.. 2^31-1
+  def nonNegativeInt(rng: RNG): (Int, RNG) =
+    val (valueInt, nextRNG) = rng.nextInt
+    (if valueInt >= 0 then valueInt else -(valueInt + 1), nextRNG)
+
+  /*
+  -2^31 -2^31+1 -2^31+2 ..... -2 -1 0 +1 +2 ...... 2^31-3 2^31-2 2^31-1
+
+  2^31-1 2^31-2  2^31-3       +1  0
+   */
+
+  // generar double [0, 1)
+  def double(rng: RNG): (Double, RNG) =
+    val (valueInt, nextRng) = nonNegativeInt(rng)
+    val valueDouble: Double = valueInt / (Int.MaxValue.toDouble + 1.0)
+    (valueDouble, nextRng)
+
+  def intDouble(rng: RNG): ((Int, Double), RNG) =
+    val (valueInt, nextRng) = rng.nextInt
+    val (valueDouble, nextRng2) = double(nextRng)
+    ((valueInt, valueDouble), nextRng2)
+
+  def doubleInt(rng: RNG): ((Double, Int), RNG) =
+    val (valueInt, nextRng) = rng.nextInt
+    val (valueDouble, nextRng2) = double(nextRng)
+    ((valueDouble, valueInt), nextRng2)
+
+  def doubleInt2(rng: RNG): ((Double, Int), RNG) =
+    val ((valueInt, valueDouble), rngNext) = intDouble(rng)
+    ((valueDouble, valueInt), rngNext)
+
+  def double3(rng: RNG): ((Double, Double, Double), RNG) =
+    val (valueDouble, nextRng) = double(rng)
+    val (valueDouble2, nextRng2) = double(nextRng)
+    val (valueDouble3, nextRng3) = double(nextRng2)
+    ((valueDouble, valueDouble2, valueDouble3), nextRng3)
+
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) =
+    @annotation.tailrec
+    def go(count: Int, currentRng: RNG, l: List[Int]): (List[Int], RNG) =
+      if count > 0 then
+        val (valInt, nextRng) = currentRng.nextInt
+        go(count - 1, nextRng, valInt :: l)
+      else (l, currentRng)
+    go(count, rng, Nil)
 
   type Rand[+A] = RNG => (A, RNG)
 
@@ -26,18 +78,6 @@ object RNG:
       val (a, rng2) = s(rng)
       (f(a), rng2)
 
-  def nonNegativeInt(rng: RNG): (Int, RNG) = ???
-
-  def double(rng: RNG): (Double, RNG) = ???
-
-  def intDouble(rng: RNG): ((Int,Double), RNG) = ???
-
-  def doubleInt(rng: RNG): ((Double,Int), RNG) = ???
-
-  def double3(rng: RNG): ((Double,Double,Double), RNG) = ???
-
-  def ints(count: Int)(rng: RNG): (List[Int], RNG) = ???
-
   def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
 
   def sequence[A](rs: List[Rand[A]]): Rand[List[A]] = ???
@@ -46,7 +86,9 @@ object RNG:
 
   def mapViaFlatMap[A, B](r: Rand[A])(f: A => B): Rand[B] = ???
 
-  def map2ViaFlatMap[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
+  def map2ViaFlatMap[A, B, C](ra: Rand[A], rb: Rand[B])(
+      f: (A, B) => C
+  ): Rand[C] = ???
 
 opaque type State[S, +A] = S => (A, S)
 
