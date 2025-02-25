b2007f2bb204da670b7553c40de23b9109b99921
Juan Manuel Gimeno Illa <jmgimeno@diei.udl.cat>
2025-02-25 16:47:45 +0100

    after feb 25

--- src/main/scala/fpinscala/exercises/datastructures/List.scala
+++ src/main/scala/fpinscala/exercises/datastructures/List.scala
@@ -1,31 +1,33 @@
 package fpinscala.exercises.datastructures
 
+import scala.annotation.tailrec
+
 /** `List` data type, parameterized on a type, `A`. */
 enum List[+A]:
   /** A `List` data constructor representing the empty list. */
   case Nil
   /** Another data constructor, representing nonempty lists. Note that `tail` is another `List[A]`,
-    which may be `Nil` or another `Cons`.
+   * which may be `Nil` or another `Cons`.
    */
   case Cons(head: A, tail: List[A])
 
 object List: // `List` companion object. Contains functions for creating and working with lists.
   def sum(ints: List[Int]): Int = ints match // A function that uses pattern matching to add up a list of integers
     case Nil => 0 // The sum of the empty list is 0.
-    case Cons(x,xs) => x + sum(xs) // The sum of a list starting with `x` is `x` plus the sum of the rest of the list.
+    case Cons(x, xs) => x + sum(xs) // The sum of a list starting with `x` is `x` plus the sum of the rest of the list.
 
   def product(doubles: List[Double]): Double = doubles match
     case Nil => 1.0
     case Cons(0.0, _) => 0.0
     // case Cons(x, _) if x == 0.0 => 0.0
-    case Cons(x,xs) => x * product(xs)
+    case Cons(x, xs) => x * product(xs)
 
   def apply[A](as: A*): List[A] = // Variadic function syntax
     if as.isEmpty then Nil
-    else Cons(as.head, apply(as.tail*))
+    else Cons(as.head, apply(as.tail *))
 
   @annotation.nowarn // Scala gives a hint here via a warning, so let's disable that
-  val result: Int = List(1,2,3,4,5) match
+  val result: Int = List(1, 2, 3, 4, 5) match
     case Cons(x, Cons(2, Cons(4, _))) => x
     case Nil => 42
     case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
@@ -35,34 +37,53 @@ object List: // `List` companion object. Contains functions for creating and wor
   def append[A](a1: List[A], a2: List[A]): List[A] =
     a1 match
       case Nil => a2
-      case Cons(h,t) => Cons(h, append(t, a2))
+      case Cons(h, t) => Cons(h, append(t, a2))
 
-  def foldRight[A,B](as: List[A], acc: B, f: (A, B) => B): B = // Utility functions
+  def foldRight[A, B](as: List[A], acc: B, f: (A, B) => B): B = // Utility functions
     as match
       case Nil => acc
       case Cons(x, xs) => f(x, foldRight(xs, acc, f))
 
   def sumViaFoldRight(ns: List[Int]): Int =
-    foldRight(ns, 0, (x,y) => x + y)
+    foldRight(ns, 0, (x, y) => x + y)
 
   def productViaFoldRight(ns: List[Double]): Double =
     foldRight(ns, 1.0, _ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
 
-  def tail[A](l: List[A]): List[A] = ???
+  def tail[A](l: List[A]): List[A] = l match
+    case Nil => sys.error("tail of empty list")
+    case Cons(_, tail) => tail
 
-  def setHead[A](l: List[A], h: A): List[A] = ???
+  def setHead[A](l: List[A], h: A): List[A] = l match
+    case Nil => sys.error("setHead of empty list")
+    case Cons(_, tail) => Cons(h, tail)
 
-  def drop[A](l: List[A], n: Int): List[A] = ???
+  @tailrec
+  def drop[A](l: List[A], n: Int): List[A] = l match
+    case Nil => Nil
+    case Cons(_, tail) if n > 0 => drop(tail, n - 1)
+    case list@Cons(_, _) => list
 
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = ???
+  @tailrec
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match
+    case Nil => Nil
+    case Cons(head, tail) if f(head) => dropWhile(tail, f)
+    case list@Cons(_, _) => list
 
-  def init[A](l: List[A]): List[A] = ???
+  def init[A](l: List[A]): List[A] = l match
+    case Nil => sys.error("init of empty list")
+    case Cons(_, Nil) => Nil
+    case Cons(head, tail) => Cons(head, init(tail))
 
   // -------------------------------------
 
-  def length[A](l: List[A]): Int = ???
+  def length[A](l: List[A]): Int =
+//    l match
+//    case List.Nil => 0
+//    case List.Cons(_, tail          ) =>  1 + length(tail)
+    foldRight(l, 0, (_, length_of_tail) =>  1 + length_of_tail)
 
-  def foldLeft[A,B](l: List[A], acc: B, f: (B, A) => B): B = ???
+  def foldLeft[A, B](l: List[A], acc: B, f: (B, A) => B): B = ???
 
   def sumViaFoldLeft(ns: List[Int]): Int = ???
 
@@ -80,11 +101,11 @@ object List: // `List` companion object. Contains functions for creating and wor
 
   def doubleToString(l: List[Double]): List[String] = ???
 
-  def map[A,B](l: List[A], f: A => B): List[B] = ???
+  def map[A, B](l: List[A], f: A => B): List[B] = ???
 
   def filter[A](as: List[A], f: A => Boolean): List[A] = ???
 
-  def flatMap[A,B](as: List[A], f: A => List[B]): List[B] = ???
+  def flatMap[A, B](as: List[A], f: A => List[B]): List[B] = ???
 
   def filterViaFlatMap[A](as: List[A], f: A => Boolean): List[A] = ???
 
