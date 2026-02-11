af75f8ea7765e48f9f55c6687fa895d6d42028f7
Juan Manuel Gimeno Illa <jmgimeno@gmail.com>
2026-02-11 11:11:41 +0100

    Update Scala version to 3.8.1 and munit library to 1.2.2 in build.scala

--- build.scala
+++ build.scala
@@ -1,8 +1,8 @@
-//> using scala 3.3.4
+//> using scala 3.8.1
 
 //> using options -feature
 //> using options -deprecation
 //> using options -source:future
-//> using options -Ykind-projector:underscores
+//> using options -Xkind-projector:underscores
 
-//> using lib org.scalameta::munit:0.7.29
+//> using lib org.scalameta::munit:1.2.2
